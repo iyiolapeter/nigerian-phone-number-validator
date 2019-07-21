fee5abcdd8e6279054a54a2872eb483cb837cefb
Peter Smart <iyiolapeter@gmail.com>
2019-07-21 12:48:12 +0100

    updated dependencies and prettier formatting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 interface SanitizeOptions {
-    mode?: "passthrough" | "throwInvalid";
+	mode?: "passthrough" | "throwInvalid";
 }
 
 export const COUNTRY_CODE = "234";
@@ -8,14 +8,14 @@ export const verifyPhoneNumber = (phone: string) => {
 	return /^([0]{1}|\+?[234]{3})([7-9]{1})([0|1]{1})([\d]{1})([\d]{7})$/g.test(String(phone));
 };
 
-export const sanitizePhoneNumber = (phone: string, { mode = "throwInvalid" }:SanitizeOptions = {}) => {
-    phone = String(phone);
-    if(!verifyPhoneNumber(phone)){
-        if(mode === "throwInvalid"){
-            throw new Error("Phone number is invalid");
-        }
-        return phone;
-    }
+export const sanitizePhoneNumber = (phone: string, { mode = "throwInvalid" }: SanitizeOptions = {}) => {
+	phone = String(phone);
+	if (!verifyPhoneNumber(phone)) {
+		if (mode === "throwInvalid") {
+			throw new Error("Phone number is invalid");
+		}
+		return phone;
+	}
 	if (phone.startsWith("0") || phone.startsWith("+")) {
 		phone = phone.substring(1);
 	}
